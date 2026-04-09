8690319ce0c1751dacb5ea76293689179bffa118
extedcouD <extedcoud@gmail.com>
2026-04-09 12:27:47 +0530

    chore: push configs and mock-service for ONDC:TRV11 2.0.1

--- mock-service/src/config/mock-config/TRV11/METRO/2.0.1/on_init/generator.ts
+++ mock-service/src/config/mock-config/TRV11/METRO/2.0.1/on_init/generator.ts
@@ -2,65 +2,74 @@ import { SessionData } from "../../../session-types";
 
 const generateRandomId = () => {
 	return Math.random().toString(36).substring(2, 15);
-  };
-const transformPayments = (payments:any) => {
-return payments.map((payment:any) => {
-	return {
-		id: generateRandomId(),
-		collected_by: payment.collected_by,
-		status: "NOT-PAID",
-		type: "PRE-ORDER",
-		params: {
-			bank_code: "XXXXXXXX",
-			bank_account_number: "xxxxxxxxxxxxxx",
-		},
-		tags: modifyTags(payment.tags),
+};
+
+const transformPayments = (payments: any) => {
+	return payments.map((payment: any) => {
+		return {
+			id: generateRandomId(),
+			collected_by: payment.collected_by,
+			status: "NOT-PAID",
+			type: "PRE-ORDER",
+			params: {
+				bank_code: "XXXXXXXX",
+				bank_account_number: "xxxxxxxxxxxxxx",
+			},
+			tags: modifyTags(payment.tags),
 		};
 	});
 };
 
-const modifyTags=(tags:any)=>{
-	  return tags.map((tag:any)=>{
-		if (tag.descriptor?.code === "SETTLEMENT_TERMS"){
+const modifyTags = (tags: any) => {
+	return tags.map((tag: any) => {
+		if (tag.descriptor?.code === "SETTLEMENT_TERMS") {
 			return {
-				 ...tag,
-				list:[
+				...tag,
+				list: [
 					...tag.list,
 					{
 						descriptor: {
-						  code: "SETTLEMENT_WINDOW" 
+							code: "SETTLEMENT_WINDOW"
 						},
-						value: "P30D" 
-					  },
+						value: "P30D"
+					},
 					{
 						descriptor: {
-						  code: "SETTLEMENT_BASIS" 
+							code: "SETTLEMENT_BASIS"
 						},
-						value: "INVOICE_RECEIPT" 
-					  }
+						value: "INVOICE_RECEIPT"
+					}
 				]
-			}
+			};
 		}
-		return tag
-	  })
-}
+		return tag;
+	});
+};
+
 export async function onInitGenerator(
 	existingPayload: any,
 	sessionData: SessionData
 ) {
-	const payments = transformPayments(sessionData.payments)
+	const payments = transformPayments(sessionData.payments);
 	existingPayload.message.order.payments = payments;
-	if (sessionData.items.length > 0) {
-		existingPayload.message.order.items = sessionData.items;
+
+	// if (sessionData.items && sessionData.items.length > 0) {
+	// 	existingPayload.message.order.items = sessionData.items;
+	// }
+
+	existingPayload.message.order.items = [];
+
+	if (sessionData.fulfillments && sessionData.fulfillments.length > 0) {
+		existingPayload.message.order.fulfillments = sessionData.fulfillments;
 	}
-	if (sessionData.fulfillments.length > 0) {
-	existingPayload.message.order.fulfillments = sessionData.fulfillments;
+
+	if (sessionData.provider) {
+		existingPayload.message.order.provider = sessionData.provider;
 	}
-	if(sessionData.provider){
-		existingPayload.message.order.provider=sessionData.provider
+
+	if (sessionData.quote != null) {
+		existingPayload.message.order.quote = sessionData.quote;
 	}
-	if(sessionData.quote != null){
-	existingPayload.message.order.quote = sessionData.quote
-	}
+
 	return existingPayload;
 }
